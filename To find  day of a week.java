c2a1c270b1241e05bbfc92b5278cb8da9cbaa4b0
Shanmukha-09 <kunapareddysuresh41@gmail.com>
2024-08-12 15:34:27 +0530

    Create To find  day of a week.java

---
+++ To find  day of a week.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    switch(a)
+	    {
+	    case 1:System.out.println("monday");
+	           break;
+	    case 2:System.out.println("tuesday");
+	           break;
+	    case 3:System.out.println("wednesday");
+	           break;
+	   case 4:System.out.println("thursday");
+	           break;
+	   case 5:System.out.println("friday");
+	           break;
+	   case 6:System.out.println("Saturday");
+	           break;
+	   default:System.out.println("invalid");
+	           break;
+	    }
+	    
+	    
+	}
+}
