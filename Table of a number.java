92c34e5701b42b9d6cb8fda2c6d98db32c08661a
Shanmukha-09 <kunapareddysuresh41@gmail.com>
2024-08-12 15:25:32 +0530

    Create Table of a number.java

---
+++ Table of a number.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    int n=sc.nextInt();
+	    
+	    for(int i=1;i<=n;i++)
+	    {
+	        System.out.println(a+"x"+i+"="+a*i);
+	    }
+	    
+	    
+	}
+}
